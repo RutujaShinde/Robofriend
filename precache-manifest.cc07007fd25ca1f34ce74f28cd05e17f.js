71271965bb65a1dee206f5e6e6db2834234ebc67
Rutuja Shinde <rutuja.shinde56@gmail.com>
2019-12-21 18:21:31 +0530

    Updates

---
+++ precache-manifest.cc07007fd25ca1f34ce74f28cd05e17f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07bd2fae743ebdddbbf628fc2be9dfc",
+    "url": "/RutujaShinde/Robofriend/index.html"
+  },
+  {
+    "revision": "eeeb5023fc5755005608",
+    "url": "/RutujaShinde/Robofriend/static/css/main.055c0053.chunk.css"
+  },
+  {
+    "revision": "0beaab8d193afed4ebf2",
+    "url": "/RutujaShinde/Robofriend/static/js/2.3c805321.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/RutujaShinde/Robofriend/static/js/2.3c805321.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eeeb5023fc5755005608",
+    "url": "/RutujaShinde/Robofriend/static/js/main.13a29375.chunk.js"
+  },
+  {
+    "revision": "fa8a97fa67ec54ef3a23",
+    "url": "/RutujaShinde/Robofriend/static/js/runtime-main.bfc25883.js"
+  }
+]);
